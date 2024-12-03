7ca313658c4154080858325dd81a53604a21d577
Wilmer Ankarstrand <wilmerwa>
2024-12-03 18:22:38 +0100

    mr. malloc complete

--- HW2/Rcomp.m
+++ HW2/Rcomp.m
@@ -1,12 +1,12 @@
-function [R] = Rcomp(lambda,T,k,c) %
+function [R] = Rcomp(lambda,T,k) %
     lamT = lambda*T;
     R = zeros(k,1);
-    p = exp(-lamT);
-    R(1) = 1 - p;
+    p = zeros(k,1);
+    p(1) = exp(-lamT);
+    R(1) = 1 - p(1);
     for s=1:k-1
         s1=s+1;
-        p = p*lamT/s1;
-        R(s1) = R(s) - p;
-        disp("iteration " + s + " R s+1 " + R(s1) + " p s+1 " + p + " R s " + R(s)  +  " table value  s + 1 " + R(s1)/c + " table value s " + R(s)/c); 
+        p(s1) = lamT*p(s)/s;
+        R(s1) = R(s) - p(s1);
     end
 end
